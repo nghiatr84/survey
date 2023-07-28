c6ddf9d6b2b66ab1fc6edfd08ee53d317a05bc41
nghiatr84 <nghiabigtran@gmail.com>
2023-07-28 16:46:03 +0300

    completed index.html, base.html, submitted.html, styles.css

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/submitted')
+def submitted():
+   return render_template('submitted.html')
+
 if __name__ == '__main__':
   app.run(debug=True)
